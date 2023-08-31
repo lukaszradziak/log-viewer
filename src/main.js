d4d96183a70b1a8c330f5b2053abb2aaa7f576c4
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-08-31 23:40:34 +0200

    Prettify all files

--- src/main.js
+++ src/main.js
@@ -1,44 +1,43 @@
-import './style.css'
-import { csvToData, generateGraph, readFileContent } from './utils'
-import exampleCsv from '../assets/example.csv?raw'
+import "./style.css";
+import { csvToData, generateGraph, readFileContent } from "./utils";
+import exampleCsv from "../assets/example.csv?raw";
 
-const $uploader = document.querySelector('.uploader')
-const $viewer = document.querySelector('.viewer')
-const $graphContainer = $viewer.querySelector('.graph-container')
-const $selectNewFile = document.querySelector('.select-new-file')
+const $uploader = document.querySelector(".uploader");
+const $viewer = document.querySelector(".viewer");
+const $graphContainer = $viewer.querySelector(".graph-container");
+const $selectNewFile = document.querySelector(".select-new-file");
 
-$uploader.style.display = 'block'
-$viewer.style.display = 'none'
+$uploader.style.display = "block";
+$viewer.style.display = "none";
 
 document
-  .querySelector('input[type=file]')
-  .addEventListener('change', async (event) => {
-		let fileContent = ''
+  .querySelector("input[type=file]")
+  .addEventListener("change", async (event) => {
+    let fileContent = "";
 
-		try {
-			fileContent = await readFileContent(event.target.files[0]);
-		} catch(e){
-			alert(`Problem with opening file: ${e.toString()}`)
-		}
+    try {
+      fileContent = await readFileContent(event.target.files[0]);
+    } catch (e) {
+      alert(`Problem with opening file: ${e.toString()}`);
+    }
 
-		event.target.value = null
-		$uploader.style.display = 'none'
-		$viewer.style.display = 'block'
+    event.target.value = null;
+    $uploader.style.display = "none";
+    $viewer.style.display = "block";
 
-		const { labels, rows } = csvToData(fileContent)
-		generateGraph($graphContainer, labels, rows)
-  })
+    const { labels, rows } = csvToData(fileContent);
+    generateGraph($graphContainer, labels, rows);
+  });
 
-$selectNewFile
-	.addEventListener('click', () => {
-		$uploader.style.display = 'block'
-		$viewer.style.display = 'none'
-	})
+$selectNewFile.addEventListener("click", () => {
+  $uploader.style.display = "block";
+  $viewer.style.display = "none";
+});
 
-if(window.location.search === '?test'){
-	$uploader.style.display = 'none'
-	$viewer.style.display = 'block'
+if (window.location.search === "?test") {
+  $uploader.style.display = "none";
+  $viewer.style.display = "block";
 
-	const { labels, rows } = csvToData(exampleCsv)
-	generateGraph($graphContainer, labels, rows)
+  const { labels, rows } = csvToData(exampleCsv);
+  generateGraph($graphContainer, labels, rows);
 }
--- src/utils.js
+++ src/utils.js
@@ -1,121 +1,120 @@
-import Highcharts from 'highcharts';
-import Exporting from 'highcharts/modules/exporting';
+import Highcharts from "highcharts";
+import Exporting from "highcharts/modules/exporting";
 
 Exporting(Highcharts);
 
 export const readFileContent = (file) => {
-	const fileReader = new FileReader()
+  const fileReader = new FileReader();
 
-	return new Promise((resolve, reject) => {
-		fileReader.onerror = () => {
-			fileReader.abort()
-			reject('Problem with opening input file.')
-		}
+  return new Promise((resolve, reject) => {
+    fileReader.onerror = () => {
+      fileReader.abort();
+      reject("Problem with opening input file.");
+    };
 
-		fileReader.onload = () => {
-			resolve(fileReader.result)
-		}
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
 
-		fileReader.readAsText(file)
-	})
-}
+    fileReader.readAsText(file);
+  });
+};
 
 export const csvToData = (content) => {
-  const separator = ','
-  const lines = content.split('\n')
+  const separator = ",";
+  const lines = content.split("\n");
 
-  const labels = lines
-    .shift()
-    .trim()
-    .split(separator)
+  const labels = lines.shift().trim().split(separator);
 
-  const rows = lines.map(line => {
+  const rows = lines.map((line) => {
     return line
       .trim()
       .split(separator)
-      .map(row => {
-        return parseFloat(row) || 0
-      })
-  })
+      .map((row) => {
+        return parseFloat(row) || 0;
+      });
+  });
 
   return {
-		labels,
-		rows
-	}
-}
+    labels,
+    rows,
+  };
+};
 
 const hslToHex = (h, s, l) => {
   l /= 100;
-  const a = s * Math.min(l, 1 - l) / 100;
-  const f = n => {
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
     const k = (n + h / 30) % 12;
     const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-    return Math.round(255 * color).toString(16).padStart(2, '0');
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0");
   };
   return `#${f(0)}${f(8)}${f(4)}`;
-}
+};
 
-let graphs = []
+let graphs = [];
 
 const createGraph = (id, colors, label, series) => {
   return Highcharts.chart(id, {
     chart: {
-      zoomType: 'x',
+      zoomType: "x",
       height: 700,
     },
     rangeSelector: {
-      selected: 2
-  },
+      selected: 2,
+    },
     exporting: {
-      enabled: true
+      enabled: true,
     },
     title: {
-      text: 'CSV'
+      text: "CSV",
     },
     colors,
     tooltip: {
       valueDecimals: 2,
       split: true,
     },
-    series
-  })
-}
+    series,
+  });
+};
 
 export const generateGraph = (domElement, labels, rows) => {
-	domElement.innerHTML = ''
-  graphs = []
+  domElement.innerHTML = "";
+  graphs = [];
 
   const label = labels[0];
   const index = 0;
 
-  const $container = document.createElement('div')
-  $container.id = `graph-${index}`
-  domElement.appendChild($container)
+  const $container = document.createElement("div");
+  $container.id = `graph-${index}`;
+  domElement.appendChild($container);
 
-  let series = [{
-    data: rows.map(row => row[index] || 0),
-    lineWidth: 0.5,
-    name: label,
-  }]
+  let series = [
+    {
+      data: rows.map((row) => row[index] || 0),
+      lineWidth: 0.5,
+      name: label,
+    },
+  ];
 
   let colors = [];
-  const maxColors = 20
-  colors = [hslToHex(index%maxColors/maxColors*360, 100, 50)]
+  const maxColors = 20;
+  colors = [hslToHex(((index % maxColors) / maxColors) * 360, 100, 50)];
 
-  if(index === 0){
+  if (index === 0) {
     series = labels.map((label, index) => ({
-      data: rows.map(row => row[index] || 0),
+      data: rows.map((row) => row[index] || 0),
       lineWidth: 0.5,
       name: label,
       visible: index === 0,
-    }))
+    }));
 
     colors = [...Array(maxColors)].map((element, index) => {
-      return hslToHex(index%maxColors/maxColors*360, 100, 50)
-    })
+      return hslToHex(((index % maxColors) / maxColors) * 360, 100, 50);
+    });
   }
 
-  graphs.push(
-    createGraph($container.id, colors, label, series)
-  )
-}
+  graphs.push(createGraph($container.id, colors, label, series));
+};
